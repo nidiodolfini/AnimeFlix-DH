9b76ea9669d7528793269f483c725cc29ae1489e
Abe <abrahao.184@gmail.com>
2022-04-01 21:08:17 -0300

    carrossel1 + carrossel2

--- Home/js/script.js
+++ Home/js/script.js
@@ -1,18 +1,18 @@
 
 
-const carousel = document.querySelector(".carousel");
+const carrossel = document.querySelector(".carrossel");
 
 let button = 0;
 
 function sliderDireita() {
   if (button === 0) {
-    carousel.style.cssText =
+    carrossel.style.cssText =
       "overflow: visible;" +
       "transform: translateX(-85vw);" +
       "transition: all 0.5s ease";
     button = 1;
   } else if (button == 1) {
-    carousel.style.cssText =
+    carrossel.style.cssText =
       "overflow: visible;" +
       "transform: translateX(0vw);" +
       "transition: all 0.5s ease";
@@ -21,7 +21,7 @@ function sliderDireita() {
 }
 
 function sliderEsquerda() {
-  carousel.style.cssText =
+  carrossel.style.cssText =
     "overflow: visible;" +
     "transform: translatex(0vw);" +
     "transition: all 0.5s ease";
