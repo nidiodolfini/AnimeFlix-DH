3f4195eae4c44922df250784aaa76b4482e79371
amanda <amandareisdw@gmail.com>
2022-04-05 17:58:40 -0300

    Commit serie

---
+++ serie/data.js
@@ -0,0 +1,189 @@
+
+
+const temporada1 = [
+    {
+        img: "./img/ep_1.png",
+        class: "imagens1-1",
+        id: ".imagens1-1",
+        caixaID: "caixa1",
+        caixaIDHover: ".caixa1",
+        hover: "./img/bg-img.png",
+        nome: "1. Episódio"
+        
+    },
+    {
+        img: "./img/ep_2.png",
+        class: "imagens1-2",
+        id: ".imagens1-2",
+        caixaID: "caixa2",
+        caixaIDHover: ".caixa2",
+        hover: "./img/bg-img.png",
+        nome: "2. Episódio"
+      
+
+    },
+    {
+        img: "./img/ep_3.png",
+        class: "imagens1-3",
+        id: ".imagens1-3",
+        caixaID: "caixa3",
+        caixaIDHover: ".caixa3",
+        hover: "./img/bg-img.png",
+        nome: "3. Episódio"
+
+    },
+    {
+        img: "./img/ep_4.png",
+        class: "imagens1-4",
+        id: ".imagens1-4",
+        caixaID: "caixa4",
+        caixaIDHover: ".caixa4",
+        hover: "./img/bg-img.png",
+        nome: "4. Episódio"
+        
+    },
+    {
+        img: "./img/ep_5.png",
+        class: "imagens1-5",
+        id: ".imagens1-5",
+        caixaID: "caixa5",
+        caixaIDHover: ".caixa5",
+        hover: "./img/bg-img.png",
+        nome: "5. Episódio"
+    },
+    {
+        img: "./img/ep_6.png",
+        class: "imagens1-6",
+        id: ".imagens1-6",
+        caixaID: "caixa6",
+        caixaIDHover: ".caixa6",
+        hover: "./img/bg-img.png",
+        nome: "6. Episódio"
+    },
+    {
+        img: "./img/ep_7.png",
+        class: "imagens1-7",
+        id: ".imagens1-7",
+        caixaID: "caixa7",
+        caixaIDHover: ".caixa7",
+        hover: "./img/bg-img.png",
+        nome: "7. Episódio"
+    },
+    {
+        img: "./img/ep_8.png",
+        class: "imagens1-8",
+        id: ".imagens1-8",
+        caixaID: "caixa8",
+        caixaIDHover: ".caixa8",
+        hover: "./img/bg-img.png",
+        nome: "8. Episódio"
+    },
+    {
+        img: "./img/ep_9.png",
+        class: "imagens1-9",
+        id: ".imagens1-9",
+        caixaID: "caixa9",
+        caixaIDHover: ".caixa9",
+        hover: "./img/bg-img.png",
+        nome: "9. Episódio"
+    },
+    {
+        img: "./img/ep_10.png",
+        class: "imagens1-10",
+        id: ".imagens1-10",
+        caixaID: "caixa10",
+        caixaIDHover: ".caixa10",
+        hover: "./img/bg-img.png",
+        nome: "10. Episódio"
+    },
+]
+
+const temporada2 = [
+    {
+        img: "./img/2_ep_1.png",
+        class: ".imagens1-1",
+    },
+    {
+        img: "./img/2_ep_2.png",
+        class: ".imagens1-2",
+    },
+    {
+        img: "./img/2_ep_3.png",
+        class: ".imagens1-3",
+    },
+    {
+        img: "./img/2_ep_4.png",
+        class: ".imagens1-4",
+    },
+    {
+        img: "./img/2_ep_5.png",
+        class: ".imagens1-5",
+    },
+    {
+        img: "./img/2_ep_6.png",
+        class: ".imagens1-6",
+    },
+    {
+        img: "./img/2_ep_7.png",
+        class: ".imagens1-7",
+    },
+    {
+        img: "./img/2_ep_8.png",
+        class: ".imagens1-8",
+    },
+    {
+        img: "./img/2_ep_9.png",
+        class: ".imagens1-9",
+    },
+    {
+        img: "./img/2_ep_10.png",
+        class: ".imagens1-10",
+    },
+]
+
+const temporada3 = [
+    {
+        img: "./img/3_ep_1.png",
+        class: ".imagens1-1",
+    },
+    {
+        img: "./img/3_ep_2.png",
+        class: ".imagens1-2",
+    },
+    {
+        img: "./img/3_ep_3.png",
+        class: ".imagens1-3",
+    },
+    {
+        img: "./img/3_ep_4.png",
+        class: ".imagens1-4",
+    },
+    {
+        img: "./img/3_ep_5.png",
+        class: ".imagens1-5",
+    },
+    {
+        img: "./img/3_ep_6.png",
+        class: ".imagens1-6",
+    },
+    {
+        img: "./img/3_ep_7.png",
+        class: ".imagens1-7",
+    },
+    {
+        img: "./img/3_ep_8.png",
+        class: ".imagens1-8",
+    },
+    {
+        img: "./img/3_ep_9.png",
+        class: ".imagens1-9",
+    },
+    {
+        img: "./img/3_ep_10.png",
+        class: ".imagens1-10",
+    },
+]
+
+
+
+export {temporada1, temporada2, temporada3};
